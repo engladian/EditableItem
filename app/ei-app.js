91813213d7e1bdcae0063cb85e8099fe87e1872e
engladian <simon.franklin2@cigna.com>
2015-09-20 17:04:24 +1200

    Added the code!!

---
+++ app/ei-app.js
@@ -0,0 +1,11 @@
+﻿'use strict';
+var appModule = angular.module('eiApp',[]);
+
+appModule.directive('makeEditable', function() {
+    return {
+        restrict: 'A',
+        transclude: true,
+        templateUrl: './make-editable/make-editable.html',
+        scope: true
+    }
+});
